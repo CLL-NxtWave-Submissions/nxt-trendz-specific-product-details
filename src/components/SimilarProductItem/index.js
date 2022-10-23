97a6eafe2fcf1c3c3e92f90535242e392f43afd9
Surya T N <suryatej.nim@yahoo.com>
2022-10-23 15:01:08 +0000

    Changed name of class name set for JSX element to render similar product title. Implemented the JSX tree to render the intended UI with SimilarProductItem component, with relevant similar product details.

--- src/components/SimilarProductItem/index.js
+++ src/components/SimilarProductItem/index.js
@@ -1,1 +1,31 @@
-// Write your code here
+import './index.css'
+
+const SimilarProductItem = props => {
+  const {itemData} = props
+  const {imageUrl, title, brand, price, rating} = itemData
+
+  return (
+    <li className="similar-product-item-container">
+      <img
+        className="similar-product-item-img"
+        src={imageUrl}
+        alt={`similar product ${title}`}
+      />
+      <p className="similar-product-item-title">{title}</p>
+      <p className="similar-product-item-brand">{brand}</p>
+      <div className="similar-product-item-price-rating-container">
+        <p className="similar-product-item-price">{price}</p>
+        <p className="similar-product-item-rating">
+          {rating}{' '}
+          <img
+            className="similar-product-item-rating-star-img"
+            src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+            alt="star"
+          />
+        </p>
+      </div>
+    </li>
+  )
+}
+
+export default SimilarProductItem
