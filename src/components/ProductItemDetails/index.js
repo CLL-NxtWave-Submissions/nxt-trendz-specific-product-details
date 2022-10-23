fc4ea9c7086717b2c4a7f5370945a278b06e3e5d
Surya T N <suryatej.nim@yahoo.com>
2022-10-23 17:55:40 +0000

    Added bare-bones code to the ProductItemDetails component implementation, with imports and class component definition

--- src/components/ProductItemDetails/index.js
+++ src/components/ProductItemDetails/index.js
@@ -1,1 +1,19 @@
-// Write your code here
+import {Component} from 'react'
+import Cookie from 'js-cookie'
+
+import Header from '../Header'
+import './index.css'
+
+const productDataAPIResponseStates = {
+  initial: 'UNINITIATED',
+  loading: 'LOADING',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+}
+
+export default class ProductItemDetails extends Component {
+  state = {
+    productQuantity: 1,
+    productDataResponseStatus: productDataAPIResponseStates.initial,
+  }
+}
