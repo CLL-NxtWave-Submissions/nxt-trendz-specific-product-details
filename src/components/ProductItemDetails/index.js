76ee42401fb8a38dc02dfb99b7cdb40a766c96a0
Surya T N <suryatej.nim@yahoo.com>
2022-10-24 07:52:16 +0000

    Added code in ProductItemDetails component to render product not found view if there is a failed product API response

--- src/components/ProductItemDetails/index.js
+++ src/components/ProductItemDetails/index.js
@@ -1,5 +1,4 @@
 import {Component} from 'react'
-import Cookie from 'js-cookie'
 
 import Header from '../Header'
 import './index.css'
@@ -15,5 +14,51 @@ export default class ProductItemDetails extends Component {
   state = {
     productQuantity: 1,
     productDataResponseStatus: productDataAPIResponseStates.initial,
+  }
+
+  onContinueShopping = () => {
+    const {history} = this.props
+    history.push('./products')
+  }
+
+  renderProductNotFoundView = () => (
+    <div className="product-not-found-container">
+      <img
+        className="product-not-found-img"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-error-view-img.png"
+        alt="error view"
+      />
+      <h1 className="product-not-found-header">Product Not Found</h1>
+      <button
+        type="button"
+        className="continue-shopping-button"
+        onClick={this.onContinueShopping}
+      >
+        Continue Shopping
+      </button>
+    </div>
+  )
+
+  renderUIBasedOnProductAPIResponseStatus = productAPIResponseStatus => {
+    let finalUI = null
+
+    if (productAPIResponseStatus === productDataAPIResponseStates.failure) {
+      finalUI = this.renderProductNotFoundView()
+    }
+
+    return finalUI
+  }
+
+  render() {
+    const {productDataResponseStatus} = this.state
+
+    return (
+      <div className="product-item-details-bg-container">
+        <Header />
+        {this.renderUIBasedOnProductAPIResponseStatus(
+          productDataResponseStatus,
+        )}
+      </div>
+    )
   }
 }
